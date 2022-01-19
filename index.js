febdbc6c9b59806d6675e4ba04b2ca21ec6056f8
HeyMaroka <marisantl@hotmail.com7>
2022-01-19 05:42:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let nome = "Marina"
+let numero = 10
+let anoAtual = 2022 - numero
+console.log(anoAtual) // 2012
+console.log(anoAtual + numero) // 2022
+console.log("Estou mexendo no console.log") //Estou mexendo no console.log
+let quartaFeira = true
+console.log(typeof quartaFeira) // boolean
